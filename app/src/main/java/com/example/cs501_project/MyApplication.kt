335a99a6d9eee1d24ad19cf6f44a4ade2c7fbe98
Sukie Zhang <sukiezhang.sz@gmail.com>
2025-05-01 13:19:46 -0400

    created a LocationBasedFactoryWorker and HistoricalPlaceRepository to use with NotificationHandler - currently shows one singular fact notification because Worker is scheduled for a one-time event, will implement periodic work request

--- app/src/main/java/com/example/cs501_project/MyApplication.kt
+++ app/src/main/java/com/example/cs501_project/MyApplication.kt
@@ -3,18 +3,61 @@ package com.example.cs501_project
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.util.Log
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.*
 import com.example.cs501_project.data.database.AppDatabase
+import com.example.cs501_project.data.database.HistoricalPlaceRepository
+import com.example.cs501_project.ui.notification.LocationBasedFactWorker
+import java.util.concurrent.TimeUnit
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        createNotificationChannel()
+        HistoricalPlaceRepository.initialize(applicationContext)
+        scheduleLocationBasedFactWorker()
+        AppDatabase.getDatabase(this)
+    }
+
+    private fun createNotificationChannel() {
         val notificationChannel = NotificationChannel(
-            "notification_channel_id",
-            "Notification name",
+            "historical_facts_channel",
+            "Fact Notification",
             NotificationManager.IMPORTANCE_HIGH
         )
         val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(notificationChannel) // sets up the channel
-        AppDatabase.getDatabase(this)
+    }
+
+    private fun scheduleLocationBasedFactWorker() {
+        Log.d("MyApplication", "scheduleLocationBasedFactWorker() CALLED")
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val oneTimeWorkRequest = OneTimeWorkRequestBuilder<LocationBasedFactWorker>()
+            .setConstraints(constraints)
+            .build()
+
+        /**
+        val periodicWorkRequest = PeriodicWorkRequestBuilder<LocationBasedFactWorker>(
+            15, TimeUnit.MINUTES
+        )
+            .setConstraints(constraints)
+            .build()
+        **/
+
+        WorkManager.getInstance(applicationContext).enqueue(oneTimeWorkRequest)
+
+        /**
+        enqueueOneTimePeriodicWork(
+            "locationBasedFactWork",
+            ExistingPeriodicWorkPolicy.UPDATE,
+            oneTimeWorkRequest
+        )
+        **/
     }
 }
--- app/src/main/java/com/example/cs501_project/ui/notification/NotificationHandler.kt
+++ app/src/main/java/com/example/cs501_project/ui/notification/NotificationHandler.kt
@@ -5,31 +5,41 @@ import android.content.Context
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.example.cs501_project.R
+import com.example.cs501_project.model.HistoricalPlace
 import com.example.cs501_project.viewmodel.LocationViewModel
 import kotlin.random.Random
 
 // notification handler defines how the user can handle the notification after they receive it
 class NotificationHandler(
-    private val context: Context,
-    private val locationViewModel: LocationViewModel
+    private val context: Context
 ) {
     // notification manager responsible for handling notifications
     private val notificationManager = context.getSystemService(NotificationManager::class.java)
-    private val notificationChannelID = "notification_channel_id" // unique identifier for channel
+    private val notificationChannelId = "historical_facts_channel" // unique identifier for channel
 
-    fun showFactNotification() {
-        val facts = locationViewModel.historicalPlaces.value[0].historicalFacts[0]
-        val title = locationViewModel.historicalPlaces.value[0].geoSearchResult.title
-        Log.d("Notification Handler", facts)
-        val notification = NotificationCompat.Builder(context, notificationChannelID)
-            .setContentTitle("Did you know this about $title?")
-            .setContentText(facts)
-            .setSmallIcon(R.drawable.round_notifications_24) // using the logo as the small icon
-            .setPriority(NotificationManager.IMPORTANCE_HIGH) // sets priority as high for notification
-            .setAutoCancel(true)
-            .build() // finalizes the creation of notification
+    fun showFactNotification(place: HistoricalPlace) {
+        Log.d("Notification Handler", "showFactNotification() called")
+        try {
+            //val facts = locationViewModel.historicalPlaces.value[0].historicalFacts[0]
+            //val title = locationViewModel.historicalPlaces.value[0].geoSearchResult.title
+            val title = place.title
+            val fact = place.historicalFacts.toString()
+            Log.d("Notification Handler", fact)
+            val notification = NotificationCompat.Builder(context, notificationChannelId)
+                .setContentTitle("Did you know this about $title?")
+                .setContentText(fact)
+                .setSmallIcon(R.drawable.round_notifications_24) // using the logo as the small icon
+                .setPriority(NotificationManager.IMPORTANCE_HIGH) // sets priority as high for notification
+                .setAutoCancel(true)
+                .build() // finalizes the creation of notification
 
-        notificationManager.notify(Random.nextInt(), notification) // triggers display of notification
-        // Random.nextInt() generates a unique identifier for notification and treats each as its own instance
+            notificationManager.notify(
+                Random.nextInt(),
+                notification
+            ) // triggers display of notification
+            // Random.nextInt() generates a unique identifier for notification and treats each as its own instance
+        } catch (e: Exception) {
+            Log.e("Notification Handler", "Error showing notification: ${e.message}", e)
+        }
     }
 }
